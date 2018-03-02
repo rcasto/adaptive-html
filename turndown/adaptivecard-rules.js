88db1b2b222ffa983fd4888c31db884531a95d5b
Richie Casto <ricast@microsoft.com>
2018-03-02 15:17:45 -0800

    comment out unsupported rules for now

--- turndown/adaptivecard-rules.js
+++ turndown/adaptivecard-rules.js
@@ -8,12 +8,10 @@ rules.paragraph = {
   filter: 'p',
 
   replacement: function (content) {
-      console.log(content);
     return AdaptiveCardHelper.wrap(content);
   }
 };
 
-// Never reaches this rule
 // rules.lineBreak = {
 //   filter: 'br',
 
@@ -32,26 +30,27 @@ rules.heading = {
   }
 };
 
-rules.blockquote = {
-  filter: 'blockquote',
+// rules.blockquote = {
+//   filter: 'blockquote',
 
-  replacement: function (content) {
-    content = content.replace(/^\n+|\n+$/g, '');
-    content = content.replace(/^/gm, '> ');
-    return '\n\n' + content + '\n\n'
-  }
-};
+//   replacement: function (content) {
+//     content = content.replace(/^\n+|\n+$/g, '');
+//     content = content.replace(/^/gm, '> ');
+//     return '\n\n' + content + '\n\n'
+//   }
+// };
 
 rules.list = {
   filter: ['ul', 'ol'],
 
   replacement: function (content, node) {
-    console.log(content);
-    if (node.nodeName === 'OL') {
+    var listItemElems = AdaptiveCardHelper.unwrap(content);
+    console.log(listItemElems);
+    // if (node.nodeName === 'OL') {
 
-    } else {
+    // } else {
 
-    }
+    // }
   }
 };
 
@@ -59,59 +58,59 @@ rules.listItem = {
   filter: 'li',
 
   replacement: function (content, node, options) {
-    console.log(content);
-    return AdaptiveCardHelper.wrap(content);
+    return content;
+    // return AdaptiveCardHelper.wrap(content);
   }
 };
 
-rules.indentedCodeBlock = {
-  filter: function (node, options) {
-    return (
-      options.codeBlockStyle === 'indented' &&
-      node.nodeName === 'PRE' &&
-      node.firstChild &&
-      node.firstChild.nodeName === 'CODE'
-    )
-  },
+// rules.indentedCodeBlock = {
+//   filter: function (node, options) {
+//     return (
+//       options.codeBlockStyle === 'indented' &&
+//       node.nodeName === 'PRE' &&
+//       node.firstChild &&
+//       node.firstChild.nodeName === 'CODE'
+//     )
+//   },
 
-  replacement: function (content, node, options) {
-    return (
-      '\n\n    ' +
-      node.firstChild.textContent.replace(/\n/g, '\n    ') +
-      '\n\n'
-    )
-  }
-};
+//   replacement: function (content, node, options) {
+//     return (
+//       '\n\n    ' +
+//       node.firstChild.textContent.replace(/\n/g, '\n    ') +
+//       '\n\n'
+//     )
+//   }
+// };
 
-rules.fencedCodeBlock = {
-  filter: function (node, options) {
-    return (
-      options.codeBlockStyle === 'fenced' &&
-      node.nodeName === 'PRE' &&
-      node.firstChild &&
-      node.firstChild.nodeName === 'CODE'
-    )
-  },
+// rules.fencedCodeBlock = {
+//   filter: function (node, options) {
+//     return (
+//       options.codeBlockStyle === 'fenced' &&
+//       node.nodeName === 'PRE' &&
+//       node.firstChild &&
+//       node.firstChild.nodeName === 'CODE'
+//     )
+//   },
 
-  replacement: function (content, node, options) {
-    var className = node.firstChild.className || '';
-    var language = (className.match(/language-(\S+)/) || [null, ''])[1];
+//   replacement: function (content, node, options) {
+//     var className = node.firstChild.className || '';
+//     var language = (className.match(/language-(\S+)/) || [null, ''])[1];
 
-    return (
-      '\n\n' + options.fence + language + '\n' +
-      node.firstChild.textContent +
-      '\n' + options.fence + '\n\n'
-    )
-  }
-};
+//     return (
+//       '\n\n' + options.fence + language + '\n' +
+//       node.firstChild.textContent +
+//       '\n' + options.fence + '\n\n'
+//     )
+//   }
+// };
 
-rules.horizontalRule = {
-  filter: 'hr',
+// rules.horizontalRule = {
+//   filter: 'hr',
 
-  replacement: function (content, node, options) {
-    return '\n\n' + options.hr + '\n\n'
-  }
-};
+//   replacement: function (content, node, options) {
+//     return '\n\n' + options.hr + '\n\n'
+//   }
+// };
 
 rules.inlineLink = {
   filter: function (node, options) {
@@ -131,51 +130,51 @@ rules.inlineLink = {
   }
 };
 
-rules.referenceLink = {
-  filter: function (node, options) {
-    return (
-      options.linkStyle === 'referenced' &&
-      node.nodeName === 'A' &&
-      node.getAttribute('href')
-    )
-  },
+// rules.referenceLink = {
+//   filter: function (node, options) {
+//     return (
+//       options.linkStyle === 'referenced' &&
+//       node.nodeName === 'A' &&
+//       node.getAttribute('href')
+//     )
+//   },
 
-  replacement: function (content, node, options) {
-    var href = node.getAttribute('href');
-    var title = node.title ? ' "' + node.title + '"' : '';
-    var replacement;
-    var reference;
+//   replacement: function (content, node, options) {
+//     var href = node.getAttribute('href');
+//     var title = node.title ? ' "' + node.title + '"' : '';
+//     var replacement;
+//     var reference;
 
-    switch (options.linkReferenceStyle) {
-      case 'collapsed':
-        replacement = '[' + content + '][]';
-        reference = '[' + content + ']: ' + href + title;
-        break
-      case 'shortcut':
-        replacement = '[' + content + ']';
-        reference = '[' + content + ']: ' + href + title;
-        break
-      default:
-        var id = this.references.length + 1;
-        replacement = '[' + content + '][' + id + ']';
-        reference = '[' + id + ']: ' + href + title;
-    }
+//     switch (options.linkReferenceStyle) {
+//       case 'collapsed':
+//         replacement = '[' + content + '][]';
+//         reference = '[' + content + ']: ' + href + title;
+//         break
+//       case 'shortcut':
+//         replacement = '[' + content + ']';
+//         reference = '[' + content + ']: ' + href + title;
+//         break
+//       default:
+//         var id = this.references.length + 1;
+//         replacement = '[' + content + '][' + id + ']';
+//         reference = '[' + id + ']: ' + href + title;
+//     }
 
-    this.references.push(reference);
-    return replacement
-  },
+//     this.references.push(reference);
+//     return replacement
+//   },
 
-  references: [],
+//   references: [],
 
-  append: function (options) {
-    var references = '';
-    if (this.references.length) {
-      references = '\n\n' + this.references.join('\n') + '\n\n';
-      this.references = []; // Reset references
-    }
-    return references
-  }
-};
+//   append: function (options) {
+//     var references = '';
+//     if (this.references.length) {
+//       references = '\n\n' + this.references.join('\n') + '\n\n';
+//       this.references = []; // Reset references
+//     }
+//     return references
+//   }
+// };
 
 rules.emphasis = {
   filter: ['em', 'i'],
@@ -197,36 +196,35 @@ rules.strong = {
   }
 };
 
-rules.code = {
-  filter: function (node) {
-    var hasSiblings = node.previousSibling || node.nextSibling;
-    var isCodeBlock = node.parentNode.nodeName === 'PRE' && !hasSiblings;
+// rules.code = {
+//   filter: function (node) {
+//     var hasSiblings = node.previousSibling || node.nextSibling;
+//     var isCodeBlock = node.parentNode.nodeName === 'PRE' && !hasSiblings;
 
-    return node.nodeName === 'CODE' && !isCodeBlock
-  },
+//     return node.nodeName === 'CODE' && !isCodeBlock
+//   },
 
-  replacement: function (content) {
-    if (!content.trim()) return ''
+//   replacement: function (content) {
+//     if (!content.trim()) return ''
 
-    var delimiter = '`';
-    var leadingSpace = '';
-    var trailingSpace = '';
-    var matches = content.match(/`+/gm);
-    if (matches) {
-      if (/^`/.test(content)) leadingSpace = ' ';
-      if (/`$/.test(content)) trailingSpace = ' ';
-      while (matches.indexOf(delimiter) !== -1) delimiter = delimiter + '`';
-    }
+//     var delimiter = '`';
+//     var leadingSpace = '';
+//     var trailingSpace = '';
+//     var matches = content.match(/`+/gm);
+//     if (matches) {
+//       if (/^`/.test(content)) leadingSpace = ' ';
+//       if (/`$/.test(content)) trailingSpace = ' ';
+//       while (matches.indexOf(delimiter) !== -1) delimiter = delimiter + '`';
+//     }
 
-    return delimiter + leadingSpace + content + trailingSpace + delimiter
-  }
-};
+//     return delimiter + leadingSpace + content + trailingSpace + delimiter
+//   }
+// };
 
 rules.image = {
   filter: 'img',
 
   replacement: function (content, node) {
-      console.log(content);
     var alt = node.alt || '';
     var src = node.getAttribute('src') || '';
     return AdaptiveCardHelper.createImage(src, {
