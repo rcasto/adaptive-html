53f9172257b5581b77006b84a4bfc315aca0c0dd
Richie <rcasto@users.noreply.github.com>
2018-03-15 10:36:36 -0700

    update prebuilts

--- dist/adaptive-html.iife.js
+++ dist/adaptive-html.iife.js
@@ -937,100 +937,6 @@ function canConvert(input) {
 
 var card = new AdaptiveCards.AdaptiveCard();
 
-function toHTML(json) {
-    if (!AdaptiveCardFilter.isValidAdaptiveCardJSON(json)) {
-        throw new TypeError(JSON.stringify(json) + ' is not valid Adaptive Card JSON.');
-    }
-    card.parse(json);
-    var adaptiveHtml = card.render();
-    recurseNodeTree(adaptiveHtml, processNode);
-    return adaptiveHtml;
-}
-
-function recurseNodeTree(node, processNodeFunc) {
-    if (!node) {
-        return node;
-    }
-    _recurseNodeTree(node, processNodeFunc);
-    return node;
-}
-
-function _recurseNodeTree(node, processNodeFunc) {
-    if (typeof processNodeFunc === 'function') {
-        processNodeFunc(node);
-    }
-    if (node.hasChildNodes()) {
-        Array.prototype.slice.call(node.childNodes).forEach(function (child) {
-            return recurseNodeTree(child, processNodeFunc);
-        });
-    }
-}
-
-function processNode(node) {
-    var nodeName = node.nodeName;
-    switch (nodeName) {
-        case 'DIV':
-            if (!node.hasChildNodes()) {
-                // remove empty divs from output html
-                node.remove();
-            }
-            var headingLevel = detectHeadingLevel(node);
-            if (headingLevel) {
-                var headingFragment = HTMLUtil.createDocumentFragment();
-                var headingNode = HTMLUtil.createElement('h' + headingLevel);
-                node.querySelectorAll('p').forEach(function (pTag) {
-                    var cloneHeadingNode = headingNode.cloneNode(false);
-                    cloneHeadingNode.innerHTML = pTag.innerHTML;
-                    headingFragment.appendChild(cloneHeadingNode);
-                });
-                node.parentNode.replaceChild(headingFragment, node);
-            }
-            break;
-    }
-}
-
-/*
-    This detection assumes default host config
-    Of course this code is currently in charge of utilizing the adaptivecards layer
-    so this shouldn't be an issue currently
-
-    This currently must stay in sync with the adaptiveCardHelper.createHeadingTextBlock construction
-*/
-function detectHeadingLevel(node) {
-    var fontWeight = node.style.fontWeight;
-    var fontSize = node.style.fontSize;
-    var bolderFontWeight = '600';
-    var lighterFontWeight = '200';
-    var extraLargeFontSize = '26px';
-    var largeFontSize = '21px';
-    var mediumFontSize = '17px';
-    var defaultFontSize = '14px';
-    var smallFontSize = '12px';
-    if (fontSize === extraLargeFontSize && fontWeight === bolderFontWeight) {
-        return 1;
-    }
-    if (fontSize === largeFontSize && fontWeight === bolderFontWeight) {
-        return 2;
-    }
-    if (fontSize === mediumFontSize && fontWeight === bolderFontWeight) {
-        return 3;
-    }
-    if (fontSize === mediumFontSize && fontWeight === lighterFontWeight) {
-        return 4;
-    }
-    if (fontSize === defaultFontSize && fontWeight === bolderFontWeight) {
-        return 5;
-    }
-    if (fontSize === smallFontSize && fontWeight === bolderFontWeight) {
-        return 6;
-    }
-    return null;
-}
-
-var AdaptiveHtmlHelper = {
-    toHTML: toHTML
-};
-
 var turndownService = new TurndownService();
 
 /**
@@ -1045,13 +951,6 @@ function toJSON(htmlStringOrNode) {
     return turndownService.turndown(htmlStringOrNode);
 }
 
-function toHTML$1(jsonOrJsonString) {
-    if (typeof jsonOrJsonString === 'string') {
-        jsonOrJsonString = UtilityHelper.tryParseJSON(jsonOrJsonString);
-    }
-    return AdaptiveHtmlHelper.toHTML(jsonOrJsonString);
-}
-
 var index = (function () {
     // check and setup globals for node for 
     // adaptivecards library if needed
@@ -1060,8 +959,8 @@ var index = (function () {
     }
     return {
         transform: transform, // maintain original api signature of previous package versions
-        toJSON: toJSON,
-        toHTML: toHTML$1
+        toJSON: toJSON
+        // toHTML
     };
 })();
 
