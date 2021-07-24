013b1005cb6ae58333776fab748d7d89a6ddec53
Richie Casto <rcasto92@gmail.com>
2021-07-24 09:45:26 -0400

    update deps to latest, switch to use terser instead of uglify for minify

--- rollup/rollup.config.iife.js
+++ rollup/rollup.config.iife.js
@@ -1,5 +1,5 @@
 import babel from '@rollup/plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 var buildMinifiedLibrary = shouldMinify(process.argv);
 var plugins = [
@@ -10,12 +10,19 @@ var plugins = [
 ];
 
 if (buildMinifiedLibrary) {
-    plugins.unshift(uglify({
-        output: {
-            // regular expressions used to 
-            // detect licent comments
-            comments: /^!/g
-        }
+    plugins.unshift(terser({
+        format: {
+            // https://github.com/TrySound/rollup-plugin-terser#comments
+            comments: function (node, comment) {
+                var text = comment.value;
+                var type = comment.type;
+                if (type == "comment2") {
+                    // regular expressions used to
+                    // detect licent comments
+                    return /^!/gi.test(text);
+                }
+            },
+        },
     }));
 }
 
@@ -27,7 +34,7 @@ export default {
     input: "src/index.js",
     output: {
         format: "iife",
-        file:  buildMinifiedLibrary ? "dist/adaptive-html.iife.min.js" : "dist/adaptive-html.iife.js",
+        file: buildMinifiedLibrary ? "dist/adaptive-html.iife.min.js" : "dist/adaptive-html.iife.js",
         name: "AdaptiveHtml",
         globals: {
             adaptivecards: 'window.AdaptiveCards'
